03198d4fc9352e68bdd365f1f71a00b1e3411c6b
Sushil Kumar <baymax732001@gmail.com>
2022-11-05 14:13:21 +0530

    Light sidebar

--- lib/constants.dart
+++ lib/constants.dart
@@ -67,8 +67,11 @@ var myAppBar = AppBar(
         color: ReUseKit.getColor.white,
       ),
     ),
-    CircleAvatar(
-      backgroundImage: NetworkImage('https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo='),
+    InkWell(
+      onTap: (){},
+      child: CircleAvatar(
+        backgroundImage: NetworkImage('https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo='),
+      ),
     ),
     IconButton(
       onPressed: () {},
