07a65cd7780b725ad076aaf66f7d56d2ced54376
Sushil Kumar <baymax732001@gmail.com>
2022-11-07 12:40:29 +0530

    Profile Info

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:vyaparmandali/responsive/responsive_layout.dart';
 
+import 'components/ProfileInfo.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       backgroundColor: Color(0xffdde9e9),
       appBar: AppBar(),
       body: SafeArea(
           child: Stack(
+           alignment: Alignment.topCenter,
         children: [
           TopBackground(),
-          SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-            child:Column(
-             children: [
-               ProfilePanel(),
-             ],
-            ),
+          ListView(
+            children: [
+              Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white,),
+                margin: EdgeInsets.only(top: 100,left: 50,right: 50,bottom: 100),
+                height:  MediaQuery.of(context).size.height*1,
+                width:  MediaQuery.of(context).size.width*0.5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child:CircleAvatar(
+                        radius: 70,
+                        backgroundImage: AssetImage('assets/images/person.png'),
+                      ),
+                    ),
+                    ProfileInFo(),
+                  ],
+                ),
+              ),
+            ],
           ),
+
         ],
       )),
     );
@@ -36,21 +59,28 @@ class TopBackground extends StatelessWidget {
     double _width = MediaQuery.of(context).size.width;
     return Container(
       width: _width,
-      height: MediaQuery.of(context).size.height*0.5,
-      child: Image.asset("assets/images/mount.png",fit: BoxFit.cover,),
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: Image.asset(
+        "assets/images/mount.png",
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
+//Profile Panel
 class ProfilePanel extends StatelessWidget {
   const ProfilePanel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: ProfileImage()),
+      alignment: Alignment.center,
+      child: ProfileImage(),
     );
   }
-  Widget ProfileImage()=> CircleAvatar(
-      radius: 70,backgroundImage: AssetImage('assets/images/person.png'),
-  );
+
+  Widget ProfileImage() => CircleAvatar(
+        radius: 70,
+        backgroundImage: AssetImage('assets/images/person.png'),
+      );
 }
